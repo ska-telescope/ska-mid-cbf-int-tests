c9d9fef97dde25a5a24c31490a42cb72411841ea
Brandon Dos Remedios <brandon.dosremedios@mda.space>
2025-03-27 17:52:40 +0000

    CIP-3444 Fix lint issues

--- src/ska_mid_cbf_int_tests/env_management/__init__.py
+++ src/ska_mid_cbf_int_tests/env_management/__init__.py
@@ -1,4 +1,4 @@
 """Module containing code for managing environment."""
 
-from check_env_nominal_state import check_env_nominal_state  # noqa: F401
-from connect_tango_host import connect_tango_host  # noqa: F401
+from .check_env_nominal_state import check_env_nominal_state  # noqa: F401
+from .connect_tango_host import connect_tango_host  # noqa: F401
